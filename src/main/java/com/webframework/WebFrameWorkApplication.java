d992c4c277c2b8bc1bba8971197ab6a01706ff3b
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-04-08 17:37:55 +0900

    Initial commit

---
+++ src/main/java/com/webframework/WebFrameWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.webframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFrameWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFrameWorkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/webframework/entity/Courses.java
@@ -0,0 +1,2 @@
+package com.webframework.entity;public class Courses {
+}
